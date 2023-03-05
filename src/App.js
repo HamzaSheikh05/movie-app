d457aa41a399b3e453b7bfd45d01c4f7fbf5ce6e
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-05 16:19:25 +0400

    modify text of header and dynamically switch active class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Routing } from "./routes/Routing";
+import { Header, Footer } from "./components";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routing />
+      <Footer />
     </div>
   );
 }
