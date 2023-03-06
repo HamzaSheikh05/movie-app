75ec299ece032d2d3a03c80d2386842200c4af7c
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-06 19:55:05 +0400

    removed default from backToTopScroll and export it from index in components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import BackToTopScroll from "./components/BackToTopScroll";
+import { BackToTopScroll } from "./components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
