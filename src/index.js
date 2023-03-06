a3417c027841069c9361dd67ced80946d41746e0
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-06 19:51:34 +0400

    scroll to top after clicking on a link

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import BackToTopScroll from "./components/BackToTopScroll";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <BackToTopScroll />
       <App />
     </Router>
   </React.StrictMode>
