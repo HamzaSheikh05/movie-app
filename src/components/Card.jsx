c6419a045f2c2941f489c3263fd17b09e70093b0
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-06 13:51:18 +0400

    updated font color and footer font

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,11 +13,11 @@ export const Card = ({ movie }) => {
       </Link>
       <div className="p-5">
         <Link to={`/movie/${id}`}>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-3xl font-medium tracking-tight font-burtons text-gray-800 dark:text-white">
             {original_title}
           </h5>
         </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal font-serif text-gray-700 dark:text-gray-400">
           {overview}
         </p>
       </div>
