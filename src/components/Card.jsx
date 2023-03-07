21aac14daee3897c44319b8eace599680a382e2e
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-07 20:35:38 +0400

    dark mode completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,17 +7,23 @@ export const Card = ({ movie }) => {
     ? `https://image.tmdb.org/t/p/w500/${poster_path}`
     : ProfilePic;
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg animate-ping shadow dark:bg-gray-800 dark:border-gray-700 m-3">
+    <div className="flex flex-col max-w-xs rounded-lg m-6 border border-gray-200 animate-ping dark:shadow-lg dark:shadow-blue-400 dark:bg-dark-bg dark:border-gray-300">
       <Link to={`/movie/${id}`}>
-        <img className="rounded-t-lg" src={imagePath} alt="movie-images" />
+        <img
+          className="rounded-t-lg w-full h-[400px] object-fill"
+          src={imagePath}
+          alt="movie-images"
+        />
       </Link>
-      <div className="p-5">
+
+      <div className="p-3 flex flex-col items-center">
         <Link to={`/movie/${id}`}>
-          <h5 className="mb-2 text-3xl font-medium tracking-tight font-burtons text-gray-800 dark:text-white">
+          <h5 className="text-3xl mb-3 underline font-medium tracking-tight font-burtons text-gray-800 dark:text-blue-200">
             {original_title}
           </h5>
         </Link>
-        <p className="mb-3 font-normal font-serif text-gray-700 dark:text-gray-400">
+
+        <p className="mb-2 font-medium tracking-normal font-serif text-gray-700 dark:text-gray-300">
           {overview}
         </p>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <footer className="p-4 font-burtons bg-custom-bg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-dark-bg">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-blue-200">
         <Link to="/" className="hover:underline">
           DevelopedByHS
         </Link>{" "}
         Copyrights Reserved 2023
       </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-blue-200 sm:mt-0">
         <li>
           <a
             href="https://www.instagram.com/saadiatrading/"
