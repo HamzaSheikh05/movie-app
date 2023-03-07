d3aecaa30c4c8b62038ea07770f3702184c8c2d2
HamzaSheikh05 <hrsheikh02@gmail.com>
2023-03-07 21:39:32 +0400

    card light mode completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = ({ movie }) => {
     ? `https://image.tmdb.org/t/p/w500/${poster_path}`
     : ProfilePic;
   return (
-    <div className="flex flex-col max-w-xs rounded-lg m-6 border border-gray-200 animate-ping dark:shadow-lg dark:shadow-blue-400 dark:bg-dark-bg dark:border-gray-300">
+    <div className="flex flex-col max-w-xs rounded-lg m-6 text shadow-md shadow-gray-300 border border-blue-100 animate-ping dark:shadow-lg dark:shadow-blue-400 dark:bg-dark-bg dark:border-gray-300">
       <Link to={`/movie/${id}`}>
         <img
           className="rounded-t-lg w-full h-[400px] object-fill"
@@ -18,12 +18,12 @@ export const Card = ({ movie }) => {
 
       <div className="p-3 flex flex-col items-center">
         <Link to={`/movie/${id}`}>
-          <h5 className="text-3xl mb-3 underline font-medium tracking-tight font-burtons text-gray-800 dark:text-blue-200">
+          <h5 className="text-3xl mb-3 underline font-medium tracking-tight font-burtons text-red-500 dark:text-blue-200">
             {original_title}
           </h5>
         </Link>
 
-        <p className="mb-2 font-medium tracking-normal font-serif text-gray-700 dark:text-gray-300">
+        <p className="mb-2 font-medium tracking-normal font-serif text-zinc-200 dark:text-gray-50">
           {overview}
         </p>
       </div>
